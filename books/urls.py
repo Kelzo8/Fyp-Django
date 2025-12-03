1276286d161eee56a3b45e48f548a41a3a2c2aa5
James Kelly <james.kelly991@yahoo.com>
2025-12-03 19:42:16 +0000

    Add book CRUD views and URLs

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
-urlpatterns: list = []
+from . import views
+
+urlpatterns = [
+    path("books/", views.book_list, name="book_list"),
+    path("books/create/", views.book_create, name="book_create"),
+    path("books/update/<int:id>/", views.book_update, name="book_update"),
+    path("books/delete/<int:id>/", views.book_delete, name="book_delete"),
+]
 
 
--- books/views.py
+++ books/views.py
@@ -1,5 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
-# Create your views here.
+from .models import Book
 
+
+def book_list(request):
+    """Display all books."""
+    books = Book.objects.all().order_by("id")
+    return render(request, "books/book_list.html", {"books": books})
+
+
+def book_create(request):
+    """Show a form to add a new book and process form submissions."""
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        published_date = request.POST.get("published_date")
+
+        if title and author and published_date:
+            Book.objects.create(
+                title=title,
+                author=author,
+                published_date=published_date,
+            )
+            return redirect(reverse("book_list"))
+
+    return render(request, "books/book_form.html")
+
+
+def book_update(request, id):
+    """Show a form to edit an existing book and save changes."""
+    book = get_object_or_404(Book, id=id)
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        published_date = request.POST.get("published_date")
+
+        if title and author and published_date:
+            book.title = title
+            book.author = author
+            book.published_date = published_date
+            book.save()
+            return redirect(reverse("book_list"))
+
+    return render(request, "books/book_form.html", {"book": book})
+
+
+def book_delete(request, id):
+    """Show a confirmation page and delete a book when confirmed."""
+    book = get_object_or_404(Book, id=id)
+
+    if request.method == "POST":
+        book.delete()
+        return redirect(reverse("book_list"))
+
+    return render(request, "books/book_confirm_delete.html", {"book": book})
 
